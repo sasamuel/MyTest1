4cbaebe3b835e65ad7ee5aad247ae645bac944f5
sasamuel <saldin.samuel@gmail.com>
2014-10-10 22:48:35 +0530

    Update CI_Starter_Test.java

--- src/test/java/CI_Starter_Test.java
+++ src/test/java/CI_Starter_Test.java
@@ -30,7 +30,7 @@ public class CI_Starter_Test {
 				.xpath("/html/body/div[1]/div[2]/div[1]/div[1]/div[3]/div/div/div/form/fieldset[2]/div/div/div/div/div[3]/div/input[1]"));
 
 		// click to the link
-		about.sendKeys("Testing");
+		about.sendKeys("quality");
 		// wait for 5 seconds
 		Thread.sleep(5000);
 	
@@ -42,7 +42,7 @@ public class CI_Starter_Test {
 		// wait for 5 seconds
 		Thread.sleep(5000);
 		
-		WebElement link = driver.findElement(By.linkText("Black-box testing - Wikipedia, the free encyclopedia"));
+		WebElement link = driver.findElement(By.linkText("What is Quality? definition and meaning"));
 		// write out the title of the page in console
 		System.out.println(driver.getTitle());
 		
